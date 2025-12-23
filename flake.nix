4bdbee96a372cc71975afebc0fea88c4fd6dc446
Sanfrag <155372694+Sanfrag@users.noreply.github.com>
2025-12-23 12:18:34 +0800

    update

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
         {
           waterfall = pkgs.stdenv.mkDerivation {
             pname = "waterfall";
-            version = "1.0.0";
+            version = "1.0.1";
 
             src = ./.;
 
